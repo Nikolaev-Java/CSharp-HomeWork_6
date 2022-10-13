543bb1e361c762d8a6086a1944d50b692739a8f0
Denis <epselent33@gmail.com>
2022-10-13 21:15:22 +0500

    выполнил второе задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+List<int> userNumber = new List<int>();
+Console.WriteLine("Введите числа. Введите ноль чтобы закончить");
+int temp;
+do
+{
+	int.TryParse(Console.ReadLine(), out temp);
+	userNumber.Add(temp);
+} while (temp != 0);
+int countPositive = 0;
+foreach (var item in userNumber)
+{
+	if (item > 0)
+	{
+		countPositive++;
+	}
+}
+Console.WriteLine($"Количество полижительных - {countPositive}");
